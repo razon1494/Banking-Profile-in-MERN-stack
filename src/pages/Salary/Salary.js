ab857ea7e2135b447065bfc62e6adf9dbc92bf18
razon1494 <razon1494@gmail.com>
2022-04-02 16:01:14 +0600

    salary page reload fixed

--- src/pages/Salary/Salary.js
+++ src/pages/Salary/Salary.js
@@ -1,14 +1,22 @@
+import { Spinner } from "react-bootstrap";
 import useAuth from "../../context/useAuth";
 import "./Salary.css";
 const Salary = () => {
-  const { person } = useAuth();
-  console.log(person);
+  const { user, person, GetPerson } = useAuth();
+  GetPerson(user.email);
+  if (!person.email) {
+    return (
+      <div className="d-flex align-items-center justify-content-center">
+        <Spinner animation="grow" variant="danger" />
+      </div>
+    );
+  }
   const salaryList = person.salary.grandSalary;
   const { basicPay, houseRent, conveyance, Medical, houseMaintanence } =
     salaryList;
 
   return (
-    <div className="container mt-2">
+    <div className="container my-2 mb-5 salary-container">
       <h2 className="mb-5">Salary Details:</h2>
       <table class="table table-hover table-light table-responsive-sm">
         <thead></thead>
